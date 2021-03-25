68ed3c1a996bd8bee58f4e78d4452c7410bb4e3f
iHappyMarvin <vmvovan@gmail.com>
2021-03-25 22:19:52 +0300

    перенести mongoose connect

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
@@ -15,12 +21,6 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
-
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
 
 app.listen(PORT, () => {
 
