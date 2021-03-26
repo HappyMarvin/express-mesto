c465b09238056c84c5b3cc191294718432e9a389
iHappyMarvin <vmvovan@gmail.com>
2021-03-26 12:31:41 +0300

    eslint --fix

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
 app.use('/', (req, res) => {
   res.status(404).send({ message: 'Неверный адрес запроса' });
 });
