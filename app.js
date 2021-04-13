f3d7a7c249a95384b10e42ea08bb8f55091913e8
Happy Marvin <HappyMarvin@yandex.ru>
2021-04-13 15:06:20 +0300

    Подключить логирование

--- app.js
+++ app.js
@@ -6,18 +6,21 @@ const errorHandler = require('./middlewares/error-handler');
 const auth = require('./middlewares/auth');
 const { createUser, login } = require('./controllers/users');
 const { validateSignUpBody, validateSignInBody } = require('./middlewares/validatons');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   autoIndex: true,
+  useUnifiedTopology: true,
 });
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(requestLogger);
 
 app.post('/signin', validateSignInBody, login);
 app.post('/signup', validateSignUpBody, createUser);
@@ -31,6 +34,7 @@ app.use('/', (req, res) => {
   res.status(404).send({ message: 'Неверный адрес запроса' });
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
--- errors/conflict-error.js
+++ errors/conflict-error.js
@@ -1,6 +1,6 @@
 module.exports = class ConflictError extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = 400;
+    this.statusCode = 409;
   }
 };
